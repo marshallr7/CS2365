512bb8f74455b8418cc9323d9c97e1490c90eded
marshallr7 <mryanp7@gmail.com>
2022-02-02 23:22:35 -0600

    added getRadius and checkRadiusBound methods
    Utilized checkRadiusBound in main to ensure radius meets the requirements

--- homework1/xyz/marshalldev/Main.java
+++ homework1/xyz/marshalldev/Main.java
@@ -7,11 +7,28 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
+        double radius = getRadius();
+
+        while (!checkRadiusBound(radius)) {
+            radius = getRadius();
+        }
+
+        new TestCircle(radius);
+        new TestEmployee();
+    }
+
+    private static double getRadius() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter a radius for the circle: ");
-        double radius = scan.nextDouble();
-        new TestCircle(radius);
+        return scan.nextDouble();
+    }
 
-        new TestEmployee();
+    private static boolean checkRadiusBound(double radius) {
+        if (radius > 0 && radius < 50) {
+            return true;
+        } else {
+            System.out.println("Radius must be between 1 and 49.");
+        }
+        return false;
     }
 }
